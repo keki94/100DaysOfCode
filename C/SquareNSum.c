32289b5d004e33337e2811fef9caccc041ceefe3
keki94 <quequisegarra@gmail.com>
2020-01-05 18:31:30 +0100

    Update SquareNSum.c

--- C/SquareNSum.c
+++ C/SquareNSum.c
@@ -1,3 +1,8 @@
+/* CodeWars 8 Kyu
+*
+*Complete the square sum function so that it squares each number passed into it and then sums the results together.
+*For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+*/
 #include <stddef.h>
 
 int square_sum(const int *values, size_t count){
